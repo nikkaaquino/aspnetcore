095892b9b1da44dfac8ba322245d4e20f70fa8fa
Nikka Aquino <nikkaaquino31@gmail.com>
2023-12-18 18:02:34 +0800

    ASP.net Core Onion Architecture

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is the info message from our values controller");
+            _logger.LogDebug("Here is debug message from our values controller");
+            _logger.LogWarn("Here is warn message from our values controller");
+            _logger.LogError("Here is an error message from our values controller");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Contract;
+using LoggerService;
+using Repository;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) => 
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+    }
+}
